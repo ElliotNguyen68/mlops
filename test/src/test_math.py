03933fd9c8f42e576bfd14fa346935e7a4159618
loc <tanlocnguyen296@gmail.com>
2023-08-20 15:49:28 +0700

    1st

---
+++ test/src/test_math.py
@@ -0,0 +1,17 @@
+from src import math
+
+def test_add(
+):
+    assert math.add(1,4)==5
+    assert math.add(5,4)==9
+    assert math.add(1,0)==1
+    
+def test_minus(
+):
+    assert math.minus(1,4)==-3
+    assert math.minus(5,4)==1
+    assert math.minus(1,0)==1
+
+
+
+
